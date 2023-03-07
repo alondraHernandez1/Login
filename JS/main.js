32603850a733404b464c059be15e6ca20c12dc7a
alondraHernandez1 <82334666+alondraHernandez1@users.noreply.github.com>
2023-03-07 14:45:41 -0600

    Update main.js

--- JS/main.js
+++ JS/main.js
@@ -1,1 +1,12 @@
+const form = document.getElementById('formulario');
+const usuario = document.getElementById('usuario');
+const contraseña = document.getElementById('contraseña');
 
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+  if (usuario.value === 'usuario' && contraseña.value === 'contraseña') {
+    alert('Inicio de sesión exitoso!');
+  } else {
+    alert('Nombre de usuario o contraseña incorrectos');
+  }
+});
